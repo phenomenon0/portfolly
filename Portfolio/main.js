179d948acfbb8ebc1748633fa88cb1b00188781c
Omen <omen@localhost.localdomain>
2021-06-24 07:59:42 +0100

    changed three to cdn

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,8 +1,9 @@
 import './style.css'
 
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'; 
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'; 
+import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
+
+//import {GLTFLoader} from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader';
 
 
 
